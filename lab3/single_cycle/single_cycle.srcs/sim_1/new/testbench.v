bc12b3a3ca3cc3fd1d5e4710301989ea21e8f31a
IFpop <965529474@qq.com>
2020-09-11 23:27:56 +0800

    fix(lab3): rstn 0->1

--- lab3/single_cycle/single_cycle.srcs/sim_1/new/testbench.v
+++ lab3/single_cycle/single_cycle.srcs/sim_1/new/testbench.v
@@ -3,9 +3,9 @@
 module testbench();
     reg rstn, clk;
     initial begin
-        rstn = 1'b1;
+        rstn = 1'b0;
         clk = 1'b0;
-        #100 rstn = 1'b0;
+        #100 rstn = 1'b1;
     end
 
     always #5 clk = ~clk;
